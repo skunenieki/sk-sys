ddd65849fab1480af0ea33b2080a32367521caca
Martins Sipenko <martins.sipenko@gmail.com>
2015-06-02 19:29:13 +0000

    Copy fonts

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ elixir(function(mix) {
        .ngHtml2Js('resources/assets/partials/**/*.{htm,html}', 'resources/assets/js')
        .browserify('index.js')
        .version(['css/app.css', 'js/bundle.js'], 'public')
+       .copy('node_modules/bootstrap/fonts', 'public/fonts')
+       .copy('node_modules/bootstrap/fonts', 'public/build/fonts')
        .copy('node_modules/angular-bootstrap/template', 'public/template')
        .copy('node_modules/angular-bootstrap/template', 'public/build/template');
 });
