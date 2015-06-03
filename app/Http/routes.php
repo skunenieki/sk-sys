eb35621ae89a326d14ccb64dc00fe8b639e5ad05
Martins Sipenko <martins.sipenko@gmail.com>
2015-06-03 23:31:13 +0300

    Added path to dump database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Response;
+
 $app->get('/', function() {
     return view('index', [
         'x' => 'zzz',
@@ -9,3 +11,10 @@ $app->get('/', function() {
 $app->get('authenticate', 'Skunenieki\System\Http\Controllers\AuthenticationController@authenticate');
 
 $app->get('participants', 'Skunenieki\System\Http\Controllers\ParticipantController@index');
+
+$app->get('mysqldump', function() {
+    $url = parse_url(env('CLEARDB_DATABASE_URL'));
+    $dump = new \MySQLDump(new \mysqli($url['host'], $url['user'], $url['pass'], substr($url['path'], 1)));
+
+    return $dump->write();
+});
