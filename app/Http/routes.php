446a914edfb06ba27d23ee37c5085c76a8ca5b5b
Martins Sipenko <martins.sipenko@accenture.com>
2015-06-05 07:25:03 +0000

    Fixed route colision

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,8 +14,8 @@ $app->get('authenticate', 'Skunenieki\System\Http\Controllers\AuthenticationCont
 $app->get('participants', 'Skunenieki\System\Http\Controllers\ParticipantController@index');
 
 $app->get('10km', 'Skunenieki\System\Http\Controllers\IdividualController@index');
-$app->get('10km/{id}', 'Skunenieki\System\Http\Controllers\IdividualController@show');
 $app->get('10km/statistics', 'Skunenieki\System\Http\Controllers\IdividualController@statistics');
+$app->get('10km/{id}', 'Skunenieki\System\Http\Controllers\IdividualController@show');
 $app->delete('10km/{id}', 'Skunenieki\System\Http\Controllers\IdividualController@destroy');
 $app->post('10km', 'Skunenieki\System\Http\Controllers\IdividualController@store');
 
