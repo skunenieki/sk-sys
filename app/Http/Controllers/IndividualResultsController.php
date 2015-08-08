9521634815b4082fd593d3bdd35e2c445831632b
Martins Sipenko <martins.sipenko@gmail.com>
2015-08-08 16:53:41 +0300

    Fixed title

--- app/Http/Controllers/IndividualResultsController.php
+++ app/Http/Controllers/IndividualResultsController.php
@@ -142,7 +142,7 @@ class IndividualResultsController extends Controller
             'SK'                       => 'Sieviešu rezultāti - kopvērtējums',
             'VK'                       => 'Vīriešu rezultāti - kopvērtējums',
             'AK'                       => 'Ārpus Konkurences',
-            'BV'                       => 'zēni, kas dzimušas, sākot ar '.$this->ageRangesForGroup($eventYear, 'BS')['birthYears']['min'].'. gadu un jaunāki',
+            'BV'                       => 'zēni, kas dzimuši, sākot ar '.$this->ageRangesForGroup($eventYear, 'BS')['birthYears']['min'].'. gadu un jaunāki',
             'S'                        => '',
             'CV'                       => 'vīrieši, kas dzimuši no '.$this->ageRangesForGroup($eventYear, 'CV')['birthYears']['min'].'. līdz '.$this->ageRangesForGroup($eventYear, 'CV')['birthYears']['max'].'. gadam; ceļa velosipēdi',
             'CV 1'                     => 'vīrieši, kas dzimuši no '.$this->ageRangesForGroup($eventYear, 'CV 1')['birthYears']['min'].'. līdz '.$this->ageRangesForGroup($eventYear, 'CV 1')['birthYears']['max'].'. gadam; ceļa velosipēdi',
